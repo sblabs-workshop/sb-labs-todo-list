0cc7efab6eb8f4ac32b80b52481c865a9c99e149
Earnest John Gungon <ej.gungon@springboardph.com>
2025-04-30 20:39:02 +0800

    Change to Product Crud

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function edit($id)
+    {
+        return view('products.edit');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+  return view('products.index');
+});
 
-Route::get("/", \App\Http\Controllers\TodoListController::class . "@index")->name("todolist.index");
+Route::get("/products", \App\Http\Controllers\ProductController::class . "@index")->name("products.index");
+Route::get("/add-product", \App\Http\Controllers\ProductController::class . "@create")->name("products.create");
+Route::get("/edit-product/{id}", \App\Http\Controllers\ProductController::class . "@edit")->name("products.edit");
